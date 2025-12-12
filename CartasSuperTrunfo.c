b79158945d0db72f7c7875b5a411c7e111320018
RAUL2799 <raulpinheiro95@gmail.com>
2025-12-12 00:08:15 +0000

    desafiointermediario

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,7 @@ int main(){
     char carta1[10], carta2[10];
     int população1,população2 , nºpontos_turisticos1, nºpontos_turisticos2;
     char Estado1[10],Estado2[10], codigo1[10],codigo2[10], nome_cidade1[20], nome_cidade2[10];
-    float area1, pib1, area2, pib2;
+    float area1, pib1, area2, pib2, densidade_populacional, pib_percapita1, densidade_populacional2,pib_percapita2;
 //Inicializei todas as variaveis que fazem parte do programa.
 
     printf("Carta 1:\n");
@@ -13,7 +13,7 @@ int main(){
     scanf("%s",codigo1);
     printf("Digite o nome da cidade:\n");
     scanf("%s", nome_cidade1);
-    printf("digite o numero populacional:");
+    printf("digite o numero populacional:\n");
     scanf("%d",&população1);
     printf("Digite o pib:\n");
     scanf("%f",&pib1);
@@ -25,13 +25,13 @@ int main(){
 /*para pedir ao usuario os dados das cartas, solicitei que o valor fosse digitado a medida que fosse lido
  para cada valor, ja gravei com o scanf. pulei uma linha a partir do valor do pib*/      
     printf("Carta 2:\n");
-    printf("Digite o Estado:");
+    printf("Digite o Estado:\n");
     scanf("%s",Estado2);
-    printf("Digite o codigo da carta:");
+    printf("Digite o codigo da carta:\n");
     scanf("%s",codigo2);
     printf("Digite o nome da cidade:\n");
     scanf("%s", nome_cidade2);
-    printf("digite o numero populacional:");
+    printf("digite o numero populacional:\n");
     scanf("%d",&população2);
     printf("Digite o pib:\n");
     scanf("%f",&pib2);
@@ -39,6 +39,11 @@ int main(){
     scanf("%f",&area2);
     printf("digite o numero de pontos turisticos:");
     scanf("%d",&nºpontos_turisticos2);
+    densidade_populacional= (float) população1/area1;
+    densidade_populacional2 = (float) população2/area2;
+    pib_percapita1 = (float) pib1/população1;
+    pib_percapita2 = (float) pib2/população2;
+
 /*Repeti o mesmo pensamento de codigo da primeira carta para fazer a segunda carta. solicitando e lendo os valores do usuário*/
 
     
@@ -46,7 +51,11 @@ int main(){
     
     
     printf("carta 1:\n - Estado:%s\n - Codigo:%s\n - nome da cidade:%s\n - população:%d\n - pib:%.5f\n - area:%.5f\n - pontos turisticos:%d\n", Estado1,codigo1,nome_cidade1,população1,pib1,area1,nºpontos_turisticos1);
+    printf("- A densidade populacional é:%f\n",densidade_populacional);
+    printf("  O pib percapita é:%f\n", pib_percapita1);
     printf("carta 2:\n - Estado:%s\n - Codigo:%s\n - nome da cidade:%s\n - população:%d\n - pib:%.5f\n - area:%.5f\n - pontos turisticos:%d\n", Estado2,codigo2,nome_cidade2,população2,pib2,area2,nºpontos_turisticos2);
+    printf("- A densidade populacional é:%f\n",densidade_populacional2);
+    printf("  O pib percapita é:%f\n", pib_percapita2);
 
 /*Li ao final do programa as informações dadas pelo usuario*/
 
@@ -55,8 +64,6 @@ int main(){
 
 
     return 0;
-
-
 
 }
 
